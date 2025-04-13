d5e393633f62bc21169ae1cf9236b26a185bd56a
Peanut Cut <121323138+Zoxycamente@users.noreply.github.com>
2025-04-13 12:04:48 -0300

    GEMAFUNK - 4EVER UPDATE
    - removi tudo do jogo base
    - adicionei umas coisas do mod
    - portei a 4ever 100% (eu acho)
    
    TO DO:
    portar o resto das músicas
    sombra na parede da 4ever

---
+++ assets/data/stages/forever.hx
@@ -0,0 +1,48 @@
+var path:String = "stages/4ever/";
+var everchange:Bool = false;
+
+var zoomTween:FlxTween;
+var colorShader = new CustomShader("adjustColor");
+
+var VALUE;
+
+function create() {
+	defaultCamZoom = 0.7;
+}
+function postCreate() {
+    colorShader.hue = 10;
+	colorShader.saturation = 10;
+	colorShader.brightness = 10;
+
+    camGame.addShader(colorShader);
+	bg1 = new FlxSprite(-600, -130).loadGraphic(Paths.image(path + "4everback"));
+    bg1.antialiasing = true;
+	insert(0, bg1);
+
+    bg2 = new FlxSprite(-970, -870).loadGraphic(Paths.image(path + "parte2"));
+	bg2.scale.set(0.8,0.8);
+    bg2.antialiasing = true;
+
+	radio = new FlxSprite(-900, -700).loadGraphic(Paths.image(path + "radio"));
+	radio.scale.set(0.4,0.4);
+    radio.antialiasing = true;
+
+}
+
+function stepHit(curStep) {
+    switch(curStep) {
+        case 1193:
+            zoomTween = FlxTween.tween(camGame, {zoom: 1.2}, 6, {ease: FlxEase.backinOut});
+        case 1249:
+            colorShader.hue = -30;
+	        colorShader.saturation = 20;
+	        colorShader.brightness = -30;
+            zoomTween.cancel();
+            for (i in [dad, boyfriend]) i.y -= 200;
+            boyfriend.x += 300;
+            camGame.flash(FlxColor.BLACK, 40);
+            add(radio);
+            insert(0, bg2);
+            remove(bg1);
+    }
+}
---
+++ assets/songs/note.hx
@@ -0,0 +1,7 @@
+function onNoteCreation(event) {
+    //event.noteSprite = 'game/notes/notes';
+}
+
+function onStrumCreation(event) {
+    //event.sprite = 'game/notes/notes';
+}
